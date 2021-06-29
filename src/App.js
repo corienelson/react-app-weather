b15abf55856f03596ce71488f96435048a88ed88
corienelson <82166983+corienelson@users.noreply.github.com>
2021-06-29 08:19:24 -0700

    bootstrap

--- src/App.js
+++ src/App.js
@@ -4,16 +4,15 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      CLN Weather App
-      <footer>
-        This project was coded by Corie Nelson and is{" "}
-        <a
-          href="https://github.com/corienelson/react-app-weather"
-          target="_blank"
-        >
-          open-sourced on github
-        </a>
-      </footer>
+      <div className="container">
+        CLN Weather App
+        <footer>
+          This project was coded by Corie Nelson and is{" "}
+          <a href="https://github.com/corienelson/react-app-weather">
+            open-sourced on github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
