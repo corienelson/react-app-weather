f31057da18bde4e8e9d715cb8c04e9988aea05e9
corienelson <82166983+corienelson@users.noreply.github.com>
2021-06-30 09:04:47 -0700

    add SearchWeather.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React from "react";
+import ReactDom from "react-dom";
 import "./App.css";
+import SearchWeather from "./SearchWeather";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-        CLN Weather App
+        <h1>Weather App</h1>
+        <SearchWeather />
+
         <footer>
           This project was coded by Corie Nelson and is{" "}
           <a href="https://github.com/corienelson/react-app-weather">
-            open-sourced on github
+            open-sourced on GitHub
           </a>
         </footer>
       </div>
